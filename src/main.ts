80d1e648a95d7bbcb19a60958387b6bc5489aae4
Ivan Luís Führ <dev.ivanfuhr@gmail.com>
2024-01-24 01:46:54 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [/localhost/, /127\.0\.0\.1/, /192\.168\.\d{1,3}\.\d{1,3}/],
+    origin: true,
   });
   await app.listen(3000);
 }
