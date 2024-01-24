d37a1431c5a9f4c7c56cd03be12acd721b728983
Ivan Luís Führ <dev.ivanfuhr@gmail.com>
2024-01-24 01:28:41 -0300

    add docker configs and insomnia requests

---
+++ Dockerfile
@@ -0,0 +1,48 @@
+# Etapa 1: Construir a aplicação
+FROM node:20.10-alpine as builder
+
+# Definir diretório de trabalho
+WORKDIR /usr/src/app
+
+# Instalar o pnpm
+RUN npm install -g pnpm
+
+# Copiar os arquivos do projeto
+COPY package*.json ./
+COPY pnpm-lock.yaml ./
+COPY prisma ./prisma/
+
+# Instalar dependências
+RUN pnpm install --frozen-lockfile
+
+# Copiar o restante dos arquivos do projeto
+COPY . .
+
+# Gerar o cliente Prisma
+RUN pnpm prisma generate
+
+# Construir o aplicativo
+RUN pnpm run build
+
+# Etapa 2: Executar a aplicação
+FROM node:20.10-alpine
+
+# Definir diretório de trabalho
+WORKDIR /usr/src/app
+
+# Instalar o pnpm
+RUN npm install -g pnpm
+
+# Copiar artefatos de construção do estágio anterior
+COPY --from=builder /usr/src/app/dist ./dist
+COPY --from=builder /usr/src/app/node_modules ./node_modules
+COPY --from=builder /usr/src/app/package*.json ./
+COPY --from=builder /usr/src/app/pnpm-lock.yaml ./
+COPY --from=builder /usr/src/app/prisma ./prisma/
+COPY --from=builder /usr/src/app/.env ./
+
+# Expor a porta em que a aplicação estará ouvindo
+EXPOSE 3000
+
+# Executar a aplicação
+CMD ["node", "dist/main"]
---
+++ src/resources/users/schemas/create-user.schema.ts
@@ -0,0 +1,42 @@
+import { FormatCpf } from 'src/common/helpers/format-cpf';
+import { ValidadeCpf } from 'src/common/helpers/validade-cpf';
+import { z } from 'zod';
+
+export const singupUserSchema = z.object({
+  name: z
+    .string({ required_error: 'O nome é obrigatório' })
+    .min(2, {
+      message: 'O nome deve ter pelo menos 2 caracteres',
+    })
+    .max(255, {
+      message: 'O nome deve ter no máximo 255 caracteres',
+    }),
+  email: z
+    .string({ required_error: 'O email é obrigatório' })
+    .email({
+      message: 'O email deve ser um email válido',
+    })
+    .min(2, {
+      message: 'O email deve ter pelo menos 2 caracteres',
+    })
+    .max(255, {
+      message: 'O email deve ter no máximo 255 caracteres',
+    }),
+  password: z
+    .string({ required_error: 'A senha é obrigatória' })
+    .min(8, {
+      message: 'A senha deve ter pelo menos 8 caracteres',
+    })
+    .max(255, {
+      message: 'A senha deve ter no máximo 255 caracteres',
+    }),
+  document: z
+    .string({ required_error: 'O CPF é obrigatório' })
+    .refine(ValidadeCpf, {
+      message: 'O CPF deve ser válido',
+    })
+    .transform(FormatCpf),
+  role: z.enum(['ADMIN', 'USER']).default('USER'),
+});
+
+export type SingupUserDto = z.infer<typeof singupUserSchema>;
